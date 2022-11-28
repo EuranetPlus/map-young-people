84e69b039e9d7cb838e7712bb8e58f97ebd4f09b
Euranet Plus <104374044+EuranetPlus@users.noreply.github.com>
2022-11-28 16:51:33 +0200

    Update config-features.js

--- src/lib/stores/config-features.js
+++ src/lib/stores/config-features.js
@@ -1,14 +1,14 @@
 export const config = {
-	mapTitle: 'europe', // The title of the map, if more than one word, separate by dash, e.g. "income-europe"
-	vercelURL: 'euranet-map-europe.vercel.app', // The url provided when deploying the map on Vercel
+	mapTitle: 'map-young-people', // The title of the map, if more than one word, separate by dash, e.g. "income-europe"
+	vercelURL: 'map-young-people.vercel.app', // The url provided when deploying the map on Vercel
 	datasetType: 'values', // "values" or "binary"; Does the data set contain numerical values or binary (0/1) values for countries?
 	datasetUnit: 'percent', // "fullNumbers" or "percent"; Is the data in percent (0.25 of GDP) or full numbers (25 people)?
 	percentRounded: false, // true or false; Should the percent values be rounded to full numbers (i.e. 26%) or not (i.e. 25.89%)
-	colourScheme: 'blue', // one of the following: "blue", "purple-blue", "green-blue", "orange-red"
+	colourScheme: 'purple-blue', // one of the following: "blue", "purple-blue", "green-blue", "orange-red"
 	headlineAvailable: true, // true or false; Should the map have a headline?
 	subheadlineAvailable: true, // true or false; Should the map have a subheadline?
 	tooltipAvailable: true, // true or false; Should the map show a tooltip when hovering over a country?
-	scaleBarAvailable: false, // true or false; Should the map show a scale bar on the top?
+	scaleBarAvailable: true, // true or false; Should the map show a scale bar on the top?
 	legendAvailable: true, // true or false; Should the map show a legend in the bottom right corner?
 	textSourceAvailable: true, // true or false; Should the map show a source text below the map?
 	textNoteAvailable: true, // true or false; Should the map show a text note below the map?
